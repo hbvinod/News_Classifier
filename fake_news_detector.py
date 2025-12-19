14b45165890385370226ead23548baec5ead4b12
Vinod B Halagi <hbvinod171@gmail.com>
2025-12-19 23:21:49 +0530

    Update fake_news_detector.py

--- fake_news_detector.py
+++ fake_news_detector.py
@@ -8,7 +8,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 
 
-NEWS_API_KEY = "Your News API "  
+NEWS_API_KEY = "Your News API Key "  
 
 
 def clean_text(text):
